4df8bb56edbe7cb511cfb6e8e1652bc289443893
Eric McKinney <mckinney99@gmail.com>
2017-10-10 18:11:32 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,14 +3,11 @@
 
 
 def display_board(board = [" "," "," "," "," "," "," "," "," "])
-row_1 = "   |   |   "
-row_2 = "-----------"
-
-puts row_1
-puts row_2
-puts row_1
-puts row_2
-puts row_1
+puts "  #{display_board[0]}  |  #{display_board[1]}  |  #{display_board[2]}  "
+puts "-----------"
+puts "  #{display_board[3]}  |  #{display_board[4]}  |  #{display_board[5]}  "
+puts "-----------"
+puts "  #{display_board[6]}  |  #{display_board[7]}  |  #{display_board[8]}  "
 
 end
 
