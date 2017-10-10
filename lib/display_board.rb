fed12cc969d74904b20cfb12276ad5270ee24b6d
Eric McKinney <mckinney99@gmail.com>
2017-10-10 18:40:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,12 +8,12 @@ puts "-----------"
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
 def display_board(board =
-  puts "   |   |   "
+  [puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
-  )
+  ])
 end
 
 display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
